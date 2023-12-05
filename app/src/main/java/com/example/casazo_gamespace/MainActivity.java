a7ba5505d160d24e10670a47b485940136242b36
Nicole Castro <oblivionshikigami97@gmail.com>
2023-12-05 19:44:59 +0800

    icon

--- app/src/main/java/com/example/casazo_gamespace/MainActivity.java
+++ app/src/main/java/com/example/casazo_gamespace/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        play = findViewById(R.id.playButton);
+        //play = findViewById(R.id.playButton);
 
         SwipeGameView swipeGameView = new SwipeGameView(this, null);
 
